dbaf8cc77c9612cb4638760d9a8eee47c6c242cf
Arian <arian.kulmer@web.de>
2022-01-07 12:34:30 +0100

    Use some variables

--- fastboot_flash_images.sh
+++ fastboot_flash_images.sh
@@ -1,9 +1,14 @@
+# User-specific variables
+REMOTE_HOST="arian@138.201.123.197"
+REMOTE_HOST_ANDROID_ROOT="/home/arian/lineage-18.1/"
+DEVICE="renoir"
+
 PARTITIONS="boot dtbo odm product system_ext system vbmeta vbmeta_system vendor_boot vendor"
 
 get_images () {
     for partition in ${PARTITIONS}; do
         echo "Downloading ${partition}"
-        rsync -v arian@138.201.123.197:/home/arian/lineage-18.1/out/target/product/renoir/${partition}.img ./
+        rsync -v ${REMOTE_HOST}:${REMOTE_HOST_ANDROID_ROOT}/out/target/product/${DEVICE}/${partition}.img ./
         echo "Downloaded ${partition}"
     done
 }
@@ -13,7 +18,7 @@ flash_images () {
         until [[ -f ./${partition}.img ]]; do
             sleep 1
         done
-        if [[ ! $(ssh arian@138.201.123.197 'sha1sum /home/arian/lineage-18.1/out/target/product/renoir/'${partition}'.img' | awk '{print $1}') = $(sha1sum ./${partition}.img | awk '{print $1}') ]]; then
+        if [[ ! $(ssh ${REMOTE_HOST} 'sha1sum '${REMOTE_HOST_ANDROID_ROOT}'/out/target/product/'${DEVICE}'/'${partition}'.img' | awk '{print $1}') = $(sha1sum ./${partition}.img | awk '{print $1}') ]]; then
             echo "${partition} download is corrupt!"
             continue
         fi
